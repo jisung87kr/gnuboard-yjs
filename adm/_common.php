d78d248eca381b6f306917123febb62ea54cdcc3
Yoojiseong <jisung87kr@gmail.com>
2019-06-29 13:40:44 +0900

    관리자 게시판 상수추가

--- adm/_common.php
+++ adm/_common.php
@@ -6,4 +6,9 @@ include_once(G5_ADMIN_PATH.'/admin.lib.php');
 if( isset($token) ){
     $token = @htmlspecialchars(strip_tags($token), ENT_QUOTES);
 }
+
+
+// adm/bbs 상수추가
+define('G5_ADM_BBS_URL', G5_ADMIN_URL.'/bbs');
+define('G5_ADM_BBS_PATH', G5_ADMIN_PATH.'/bbs');
 ?>
