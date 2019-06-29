05b5a0236aa1841d59de24c51fb920b27f14fbf1
Yoojiseong <jisung87kr@gmail.com>
2019-06-29 13:44:27 +0900

    .

--- adm/admin.menu1000.php
+++ adm/admin.menu1000.php
@@ -1,9 +1,9 @@
 <?php
 $menu["menu1000"] = array (
-    array('1000000', '게시글관리', ''.G5_ADM_BBS_URL.'/board.php?bo_table=free', 'sms5'),
-    array('1000100', '자유게시판', ''.G5_ADM_BBS_URL.'/board.php?bo_table=free', 'sms5_config'),
-    array('1000200', '겔러리', ''.G5_ADM_BBS_URL.'/board.php?bo_table=gallery', 'sms5_mb_update'),
-    array('1000300', '공지사항', ''.G5_ADM_BBS_URL.'/board.php?bo_table=notice', 'sms_write'),
-    array('1000400', '질문과답변', ''.G5_ADM_BBS_URL.'/board.php?bo_table=qa', 'sms_write'),
+    array('1000000', '게시글관리', ''.G5_SMS5_ADMIN_URL.'/board.php?bo_table=free', 'sms5'),
+    array('1000100', '자유게시판', ''.G5_SMS5_ADMIN_URL.'/board.php?bo_table=free', 'sms5_config'),
+    array('1000200', '겔러리', ''.G5_SMS5_ADMIN_URL.'/board.php?bo_table=gallery', 'sms5_mb_update'),
+    array('1000300', '공지사항', ''.G5_SMS5_ADMIN_URL.'/board.php?bo_table=notice', 'sms_write'),
+    array('1000400', '질문과답변', ''.G5_SMS5_ADMIN_URL.'/board.php?bo_table=ㅂㅁ', 'sms_write'),
 );
 ?>
