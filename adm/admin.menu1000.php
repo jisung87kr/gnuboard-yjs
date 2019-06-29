eef7bffc915e25c6fff56579ca0f6d2dd69b1376
Yoojiseong <jisung87kr@gmail.com>
2019-06-29 13:52:13 +0900

    url 수정

--- adm/admin.menu1000.php
+++ adm/admin.menu1000.php
@@ -1,9 +1,9 @@
 <?php
 $menu["menu1000"] = array (
-    array('1000000', '게시글관리', ''.G5_SMS5_ADMIN_URL.'/board.php?bo_table=free', 'free'),
-    array('1000100', '자유게시판', ''.G5_SMS5_ADMIN_URL.'/board.php?bo_table=free', 'free'),
-    array('1000200', '겔러리', ''.G5_SMS5_ADMIN_URL.'/board.php?bo_table=gallery', 'gallery'),
-    array('1000300', '공지사항', ''.G5_SMS5_ADMIN_URL.'/board.php?bo_table=notice', 'notice'),
-    array('1000400', '질문과답변', ''.G5_SMS5_ADMIN_URL.'/board.php?bo_table=qa', 'qa'),
+    array('1000000', '게시글관리', G5_ADM_BBS_URL.'/board.php?bo_table=free', 'free'),
+    array('1000100', '자유게시판', G5_ADM_BBS_URL.'/board.php?bo_table=free', 'free'),
+    array('1000200', '겔러리', G5_ADM_BBS_URL.'/board.php?bo_table=gallery', 'gallery'),
+    array('1000300', '공지사항', G5_ADM_BBS_URL.'/board.php?bo_table=notice', 'notice'),
+    array('1000400', '질문과답변', G5_ADM_BBS_URL.'/board.php?bo_table=qa', 'qa'),
 );
 ?>
