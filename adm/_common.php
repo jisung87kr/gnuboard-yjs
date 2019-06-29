8d6ec13a72693cf1ddec7ed0b4a0527f0e02a086
Yoojiseong <jisung87kr@gmail.com>
2019-06-29 13:51:55 +0900

    상수 선언 위치 수정

--- adm/_common.php
+++ adm/_common.php
@@ -1,14 +1,15 @@
 <?php
 define('G5_IS_ADMIN', true);
 include_once ('../common.php');
+
+// adm/bbs 상수추가
+define('G5_ADM_BBS_URL', G5_ADMIN_URL.'/bbs');
+define('G5_ADM_BBS_PATH', G5_ADMIN_PATH.'/bbs');
+// end
+
 include_once(G5_ADMIN_PATH.'/admin.lib.php');
 
 if( isset($token) ){
     $token = @htmlspecialchars(strip_tags($token), ENT_QUOTES);
 }
-
-
-// adm/bbs 상수추가
-define('G5_ADM_BBS_URL', G5_ADMIN_URL.'/bbs');
-define('G5_ADM_BBS_PATH', G5_ADMIN_PATH.'/bbs');
 ?>
