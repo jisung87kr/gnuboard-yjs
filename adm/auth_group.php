2aee9e98f604a9bfdf9a8528abcd23b6aa55db5e
Yoo <jisung87kr@gmail.com>
2019-11-14 23:19:40 +0900

    파일 생성

---
+++ adm/auth_group.php
@@ -0,0 +1,14 @@
+<?php
+$sub_menu = "201000";
+include_once('./_common.php');
+
+$g5['title'] = "관리권한설정";
+include_once('./admin.head.php');
+
+?>
+
+
+
+<?php
+include_once ('./admin.tail.php');
+?>
