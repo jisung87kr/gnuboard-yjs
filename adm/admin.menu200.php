dbf724b2270adf6b4fbf80048c1e510e41040b37
Yoo <jisung87kr@gmail.com>
2019-11-14 23:18:44 +0900

    파일 생성

--- adm/admin.menu200.php
+++ adm/admin.menu200.php
@@ -7,6 +7,7 @@ $menu['menu200'] = array (
     array('200810', '접속자검색', G5_ADMIN_URL.'/visit_search.php', 'mb_search', 1),
     array('200820', '접속자로그삭제', G5_ADMIN_URL.'/visit_delete.php', 'mb_delete', 1),
     array('200200', '포인트관리', G5_ADMIN_URL.'/point_list.php', 'mb_point'),
-    array('200900', '투표관리', G5_ADMIN_URL.'/poll_list.php', 'mb_poll')
+    array('200900', '투표관리', G5_ADMIN_URL.'/poll_list.php', 'mb_poll'),
+    array('201000', '그룹관리', G5_ADMIN_URL.'/auth_group.php', 'auth_group')
 );
 ?>
